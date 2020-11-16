41a29161cf5ca0add08add7a47fe7b926b3bf64c
Golden Thumb <zhijunsheng@mbp2020.local>
2020-11-16 13:10:29 -0500

    created new Xcode SwiftUI project named Chess

---
+++ Chess/Chess/ChessApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChessApp.swift
+//  Chess
+//
+//  Created by Golden Thumb on 2020-11-16.
+//
+
+import SwiftUI
+
+@main
+struct ChessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
