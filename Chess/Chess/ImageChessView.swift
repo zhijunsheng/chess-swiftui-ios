a64bf6f7c630768195aed6bd74f2132b753c9591
Golden Thumb <zhijunsheng@mbp2020.local>
2020-11-27 17:14:01 -0500

    created ImageChessView

---
+++ Chess/Chess/ImageChessView.swift
@@ -0,0 +1,20 @@
+//
+//  ImageChessView.swift
+//  Chess
+//
+//  Created by Golden Thumb on 2020-11-27.
+//
+
+import SwiftUI
+
+struct ImageChessView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ImageChessView_Previews: PreviewProvider {
+    static var previews: some View {
+        ImageChessView()
+    }
+}
