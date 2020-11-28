67e41c9b0bd2f109057fac88830abd8ef069056a
Golden Thumb <zhijunsheng@mbp2020.local>
2020-11-27 22:14:19 -0500

    refactored chess board drawing code with a Shape custom named "Square"

--- Chess/Chess/ImageChessView.swift
+++ Chess/Chess/ImageChessView.swift
@@ -9,32 +9,33 @@ import SwiftUI
 
 struct ImageChessView: View {
     var body: some View {
-        let originX: CGFloat = 29
-        let originY: CGFloat = 70
-        let cellSide: CGFloat = 43
-        
         ZStack {
-            Path { path in
-                for row in 0..<4 {
-                    for col in 0..<4 {
-                        path.addRect(CGRect(x: originX + CGFloat(2 * col) * cellSide, y: originY + CGFloat(2 * row) * cellSide, width: cellSide, height: cellSide))
-                        path.addRect(CGRect(x: originX + CGFloat(2 * col + 1) * cellSide, y: originY + CGFloat(2 * row + 1) * cellSide, width: cellSide, height: cellSide))
-                    }
-                }
-            }
-            .fill(Color(#colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)))
             
-            Path { path in
-                for row in 0..<4 {
-                    for col in 0..<4 {
-                        path.addRect(CGRect(x: originX + CGFloat(2 * col + 1) * cellSide, y: originY + CGFloat(2 * row) * cellSide, width: cellSide, height: cellSide))
-                        path.addRect(CGRect(x: originX + CGFloat(2 * col) * cellSide, y: originY + CGFloat(2 * row + 1) * cellSide, width: cellSide, height: cellSide))
-                    }
+            ForEach (0..<4) { row in
+                ForEach (0..<4) { col in
+                    Square(col: 2 * col + 0, row: 2 * row + 0).fill(Color(#colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)))
+                    Square(col: 2 * col + 1, row: 2 * row + 1).fill(Color(#colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)))
+                    Square(col: 2 * col + 1, row: 2 * row + 0).fill(Color(#colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)))
+                    Square(col: 2 * col + 0, row: 2 * row + 1).fill(Color(#colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)))
                 }
             }
-            .fill(Color(#colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)))
         }
     }
+}
+
+struct Square: Shape {
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        path.addRect(CGRect(x: originX + CGFloat(col) * cellSide, y: originY + CGFloat(row) * cellSide, width: cellSide, height: cellSide))
+        return path
+    }
+    
+    var col: Int
+    var row: Int
+    
+    let originX: CGFloat = 29
+    let originY: CGFloat = 70
+    let cellSide: CGFloat = 43
 }
 
 struct ImageChessView_Previews: PreviewProvider {
