6971937c30c265cb7e0db996c53e94d9a2e51664
Golden Thumb <zhijunsheng@mbp2020.local>
2020-11-27 21:43:03 -0500

    drew 8x8 chess board in black and gray

--- Chess/Chess/ImageChessView.swift
+++ Chess/Chess/ImageChessView.swift
@@ -9,7 +9,31 @@ import SwiftUI
 
 struct ImageChessView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        let originX: CGFloat = 29
+        let originY: CGFloat = 70
+        let cellSide: CGFloat = 43
+        
+        ZStack {
+            Path { path in
+                for row in 0..<4 {
+                    for col in 0..<4 {
+                        path.addRect(CGRect(x: originX + CGFloat(2 * col) * cellSide, y: originY + CGFloat(2 * row) * cellSide, width: cellSide, height: cellSide))
+                        path.addRect(CGRect(x: originX + CGFloat(2 * col + 1) * cellSide, y: originY + CGFloat(2 * row + 1) * cellSide, width: cellSide, height: cellSide))
+                    }
+                }
+            }
+            .fill(Color(#colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)))
+            
+            Path { path in
+                for row in 0..<4 {
+                    for col in 0..<4 {
+                        path.addRect(CGRect(x: originX + CGFloat(2 * col + 1) * cellSide, y: originY + CGFloat(2 * row) * cellSide, width: cellSide, height: cellSide))
+                        path.addRect(CGRect(x: originX + CGFloat(2 * col) * cellSide, y: originY + CGFloat(2 * row + 1) * cellSide, width: cellSide, height: cellSide))
+                    }
+                }
+            }
+            .fill(Color(#colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)))
+        }
     }
 }
 
